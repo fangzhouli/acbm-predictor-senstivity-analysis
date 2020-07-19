9ce44144874faaa0cc347cb37f7f2887100a8397
fangzhouli <fzli0805@gmail.com>
2020-07-18 20:36:57 -0700

    added analyze script, renamed analyzers

---
+++ analyze.py
@@ -0,0 +1,21 @@
+from analyzers import *
+
+
+def analyze_all():
+    # delta = DeltaAnalyzer()
+    dgsm = DGSMAnalyzer()
+    fast = FastAnalyzer()
+    morris = MorrisAnalyzer()
+    rbd_fast = RDBFastAnalyzer()
+    sobol = SobolAnalyzer()
+
+    # delta.analyze()
+    dgsm.analyze()
+    fast.analyze()
+    morris.analyze()
+    rbd_fast.analyze()
+    sobol.analyze()
+
+
+if __name__ == '__main__':
+    analyze_all()
