c4fdf036214d36ebc3a34a2dd109e4ddad095cad
fangzhouli <fzli0805@gmail.com>
2020-07-18 21:00:46 -0700

    added comments

--- analyze.py
+++ analyze.py
@@ -1,7 +1,24 @@
+# -*- coding: utf-8 -*-
+"""analyze.py description.
+
+This script runs all the Analyzer modules defined in 'acbm/analyzers/'.
+
+Author:
+    Fangzhou Li - https://github.com/fangzhouli
+
+"""
+
+
 from analyzers import *
 
 
 def analyze_all():
+    """Call analyze methods of all analyzers.
+
+    Generates:
+        6 analysis results in 'acbm/data/output/'.
+
+    """
     delta = DeltaAnalyzer()
     dgsm = DGSMAnalyzer()
     fast = FastAnalyzer()
